aafb9f63ddacfafc60fe3155a77abbdfe6848ba8
jeffreyou <129538387+jeffreyou@users.noreply.github.com>
2023-10-11 15:50:29 +0000

    Finished Iterations Q9 - Q10

--- unit1/iteration/iterationq6.py
+++ unit1/iteration/iterationq6.py
@@ -1,15 +1,15 @@
+# Printing nth Fibonacci Sequence 
 num = int(input())
-fibonacciNum = 2
-digit1 = 0
-digit2 = 1
+digit1 = 0 # F(0)
+digit2 = 1 # F(1)
+fibonacciNum = 0 # F(2)
 
-while fibonacciNum < num:
-    print(digit1)
-    digit1 = digit1 + digit2
-    if fibonacciNum < num:
-        print(digit2)
-        digit2 = digit1 + digit2
-    fibonacciNum += 1
+for i in range(2, num+1): # fibonacciNum  starts at F(2)
+    fibonacciNum = digit1 + digit2
+    digit1 = digit2
+    digit2 = fibonacciNum
+    
+print(fibonacciNum)
 
 
     
